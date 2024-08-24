3c767a48f40d932b08aba2ea377850038752d7e7
Aaron Ochieng <ochiengaoron@gmail.com>
2024-08-24 10:47:03 +0300

    Implement main function to start TCP chat server
    - Added `main()` function to start a TCP chat server that listens on a specified port.
    - Validated command-line arguments to ensure exactly one port argument is provided.
    - Set up a TCP listener on the given port and handled errors during the setup.
    - Started a goroutine for handling incoming messages using `utils.HandleMessages()`.
    - Accepted incoming connections in a loop and handled each connection in a separate goroutine using `utils.HandleConnection()`.
    - Included error logging for connection acceptance failures.

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	utils "net_cat/utils"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+
+	if len(args) != 1 {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+		os.Exit(0)
+	}
+
+	port := args[0]
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatalf("Failed to listen on port %s: %v", port, err)
+	}
+	defer listener.Close()
+	fmt.Printf("Listening on port %s\n", port)
+
+	go utils.HandleMessages()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("Failed to accept connection: %v", err)
+			continue
+		}
+		go utils.HandleConnection(conn)
+	}
+}
