d27fc3224caaffe0bc3a457a66f530b10b8059a4
Aaron Ochieng <ochiengaoron@outlook.com>
2024-10-02 20:03:44 +0300

    feat: Excluding some connections - Notifiying all the clients already connected in the channel with the exception of the new client. - Writing the accepted message by the function to the channel

--- utils/new_conn_notifier.go
+++ utils/new_conn_notifier.go
@@ -1,5 +1,14 @@
 package net_cat
 
-func notifyClients(notification string) {
-	messages <- notification
+import "net"
+
+func notifyClients(message string, excludeConn net.Conn) {
+	clientsMutex.Lock()
+	defer clientsMutex.Unlock()
+
+	for clientConn, client := range clients {
+		if clientConn != excludeConn { // Skip the new client
+			client.conn.Write([]byte(message + "\n"))
+		}
+	}
 }
