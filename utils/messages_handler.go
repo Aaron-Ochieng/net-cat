713ff3e256e1cb8b404090966c855eb173307499
Aaron Ochieng <ochiengaoron@gmail.com>
2024-10-03 09:02:52 +0300

    feat: store all new messages in prevmessages - Store all the messages in the channel to prevMessages slice. - The store messages will be loaded for new clients connecting to the channel

--- utils/messages_handler.go
+++ utils/messages_handler.go
@@ -3,6 +3,9 @@ package net_cat
 func HandleMessages() {
 	for message := range messages {
 		ClientsMutex.Lock()
+
+		// Store all the messages to prevMessages slice []string
+		prevMessages = append(prevMessages, message)
 		for _, client := range Clients {
 			client.conn.Write([]byte(message + "\n"))
 		}
