d85d8356df17a2b825b37291f5044ccc6105cdc5
Aaron Ochieng <ochiengaoron@gmail.com>
2024-10-03 11:28:34 +0300

    refractor: removed notifying clients functions

--- utils/remove_client.go
+++ utils/remove_client.go
@@ -1,7 +1,6 @@
 package net_cat
 
 import (
-	"fmt"
 	"net"
 )
 
@@ -9,10 +8,9 @@ func removeClient(conn net.Conn) {
 	ClientsMutex.Lock()
 	defer ClientsMutex.Unlock()
 
-	client, exists := Clients[conn]
+	_, exists := Clients[conn]
 	if exists {
 		delete(Clients, conn)
-		notifyClients(fmt.Sprintf("%s has left the chat...", client.name), nil)
 		conn.Close()
 	}
 }
