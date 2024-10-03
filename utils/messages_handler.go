23fee3fb9aeece387998b1ef7db9f276478ff0ef
Aaron Ochieng <ochiengaoron@gmail.com>
2024-10-03 09:57:36 +0300

    feat: Store and broadcast messages excluding the sender
    - Added logic to store all broadcasted messages in the `prevMessages` slice for history tracking.
    - Modified message broadcasting to skip sending the message back to the sender, ensuring only other clients receive it.
    - Used mutex locking to protect shared data (message storage and broadcasting).

--- utils/messages_handler.go
+++ utils/messages_handler.go
@@ -5,9 +5,13 @@ func HandleMessages() {
 		ClientsMutex.Lock()
 
 		// Store all the messages to prevMessages slice []string
-		prevMessages = append(prevMessages, message)
-		for _, client := range Clients {
-			client.conn.Write([]byte(message + "\n"))
+		prevMessages = append(prevMessages, message.messageText)
+		formattedMessage, senderConn := message.messageText, message.senderConn
+		for conn, client := range Clients {
+			// Skip the sender
+			if conn != senderConn {
+				client.conn.Write([]byte(formattedMessage + "\n"))
+			}
 		}
 		ClientsMutex.Unlock()
 	}
