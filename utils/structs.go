eb08f48cf83e0bfb32e83ea1386e1025a831b35f
abrakingoo <maikoabraham68@gmail.com>
2024-10-07 08:47:59 +0300

    feat: Define Server Struct to handle network connection
    - listeningAddr Addr where the server listens for incomming connection
    - listener       a net.Listener to accept client connections
    - clients       to track active clients , mapping connections to client structs
    - clientMutex   to ensure thread safe access to clients map
    - messages      A channel to manage incoming and outgoing messages btn clients
    - prevMessages to store prev messages for later retrieval

--- utils/structs.go
+++ utils/structs.go
@@ -1,6 +1,9 @@
 package net_cat
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
 type Client struct {
 	conn net.Conn
@@ -12,4 +15,13 @@ const MaxConnections = 10 // Define the maximum number of connections
 type Message struct {
 	messageText string
 	senderConn  net.Conn
+}
+
+type Server struct {
+	listeningAddr string              // Addr where the server listens for incomming connection
+	listener      net.Listener        // a net.Listener to accept client connections
+	clients       map[net.Conn]Client // To track active clients , mapping connections to client structs
+	clientMutex   sync.Mutex          // to ensure thread safe access to clients map
+	messages      chan Message        // A channel to manage incoming and outgoing messages btn clients
+	prevMessages  []string            // to store prev messages for later retrieval
 }
